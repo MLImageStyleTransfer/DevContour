1b28597bf6917ccb9c9cefb2c605c1a1fb497f27
JUSSIAR <klokov.stas.022@mail.ru>
2022-02-12 04:42:51 +0300

    Added bootstrap & some fixes

--- DemoApp/frontend/src/components/content-box/content-box.tsx
+++ DemoApp/frontend/src/components/content-box/content-box.tsx
@@ -11,6 +11,7 @@ export default function ContentBox({image, stopEdit}: Props) {
   return (
     <div className={style.ground}>
       <div className={style.content}>
+        <h2 className={style.header}>Редактор</h2>
         <div className={style.imageWrapper}>
           {Boolean(image) && <img
               src={image}
@@ -18,11 +19,30 @@ export default function ContentBox({image, stopEdit}: Props) {
               className={style.image}
           />}
         </div>
-        <hr/>
-        <br/>
-        <button onClick={stopEdit}>
-          Stop
-        </button>
+        <div className={style.manage}>
+          <div className={`${style.customSize} input-group`}>
+            <div className="input-group-prepend">
+              <div className="input-group-text">
+                <input type="checkbox" aria-label="Checkbox for following text input"/>
+              </div>
+            </div>
+            <label className="form-control" aria-label="Text input with checkbox">
+              Черно-белый фильтр
+            </label>
+          </div>
+          <button
+            onClick={stopEdit}
+            className="btn btn-warning"
+          >
+            Отмена
+          </button>
+          <button
+            onClick={stopEdit}
+            className="btn btn-success"
+          >
+            Сохранить
+          </button>
+        </div>
       </div>
     </div>
   )
--- DemoApp/frontend/src/components/load-box/load-box.tsx
+++ DemoApp/frontend/src/components/load-box/load-box.tsx
@@ -1,17 +1,31 @@
-import React, {useRef} from 'react'
+import React, {useRef, useState} from 'react'
 
 type Props = {
   loadImage: (ref: any) => void
 }
 
 export default function LoadBox({loadImage}: Props) {
+  const [chosen, setChosen] = useState<boolean>(false)
+
   const ref = useRef(null)
+
+  const checker = () => {
+    if ((ref?.current || {files: [false]}).files[0]) {
+      setChosen(true)
+    } else {
+      setChosen(false)
+    }
+  }
 
   return (
     <fieldset title="LOAD FORM">
-      <input type="file" id="file" ref={ref}/>
-      <button onClick={() => loadImage(ref)}>
-        Save
+      <input onInput={checker} type="file" id="file" ref={ref}/>
+      <button
+        onClick={() => loadImage(ref)}
+        className={`btn ${chosen ? 'btn-primary' : 'btn-secondary'}`}
+        disabled={!chosen}
+      >
+        Редактировать
       </button>
     </fieldset>
   )
